ea31cb576f56e878e332d07526bd058657d39fd7
Silaspace <silasjharvey@gmail.com>
2020-11-17 15:54:15 +0000

    I hope this works...

--- tensor.py
+++ tensor.py
@@ -2,10 +2,4 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
-model = keras.Sequential(
-    [
-        layers.Dense(2, activation="relu", name="layer1"),
-        layers.Dense(3, activation="relu", name="layer2"),
-        layers.Dense(4, name="layer3"),
-    ]
-)
+from tensorflow.keras.datasets import mnist
